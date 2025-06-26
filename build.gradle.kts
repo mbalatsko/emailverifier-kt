82f7477bdd2e8fa02b97af9c7158840a2e7e3fe7
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2025-06-26 13:54:40 +0200

    prepare gradle for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,40 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "emailverifier-kt"
+            pom {
+                name = project.name
+                description =
+                    "EmailVerifier is a composable, pluggable Kotlin library for validating email addresses beyond just their syntax. It's built with a clear focus: help developers reliably assess whether a given email is real, meaningful, and worth accepting."
+                url = "https://mbalatsko.github.io/emailverifier-kt/"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "mbalatsko"
+                        name = "Maksym Balatsko"
+                        email = "mbalatsko@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/mbalatsko/emailverifier-kt.git"
+                    developerConnection = "scm:git:ssh://github.com/mbalatsko/emailverifier-kt.git"
+                    url = "https://github.com/mbalatsko/emailverifier-kt"
+                }
+            }
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
